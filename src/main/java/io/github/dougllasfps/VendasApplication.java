faf753ff6372d50292e0c98b5224862227889141
Vithor <vithort@gmail.com>
2020-10-21 16:36:12 -0300

    4. Configurations e Beans
    Configurations e Beans

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,5 +1,6 @@
 package io.github.dougllasfps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+    @Autowired
+    private String applicationName;
+
     @GetMapping("/hello")
     public String helloWorld() {
-        return "hello world!";
+        return applicationName;
     }
 
     public static void main(String[] args) {
