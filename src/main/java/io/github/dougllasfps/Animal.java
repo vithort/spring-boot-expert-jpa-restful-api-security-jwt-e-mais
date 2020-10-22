2f8d502a8ec7c4138e05c2d04211927584d8c746
Vithor <vithort@gmail.com>
2020-10-22 17:13:25 -0300

    Limpeza do Projeto
    Limpeza do Projeto

--- src/main/java/io/github/dougllasfps/Animal.java
+++
@@ -1,5 +0,0 @@
-package io.github.dougllasfps;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/io/github/dougllasfps/repository/MeuRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.dougllasfps.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MeuRepository {
-}
