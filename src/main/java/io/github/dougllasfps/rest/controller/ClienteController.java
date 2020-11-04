59e23e3c37c8defcfef34cd95e8cfb43dd000ae4
Vithor <vithort@gmail.com>
2020-11-04 16:06:52 -0300

    Put Mapping - Atualizando um Cliente
    Put Mapping - Atualizando um Cliente

--- src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -49,5 +49,19 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity update(
+            @PathVariable Integer id,
+            @RequestBody Cliente cliente
+    ) {
+        return clientes
+                .findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
